a5c0516eedf1df4d91fc704517923ee59a3040a0
Dmitry Moskalenko <dimamikron41@gmail.com>
2022-06-30 15:14:46 +0300

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "../in.m.css";
+
+const App = () => {
+  return (
+    <>
+      <div className={styles.title}>Hello World</div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./Components/App";
+//
+
+//
+import "./index.css";
+
+ReactDOM.createRoot(document.querySelector("#app")).render(<App />);
